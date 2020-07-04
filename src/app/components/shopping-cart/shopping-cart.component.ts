ea531fe81da3f3f8066af9ec5d4df823035700a7
Valerio Risuleo <valerio.risuleo@gmail.com>
2020-07-04 20:13:58 +0100

    shopping cart: refactoring

--- src/app/components/shopping-cart/shopping-cart.component.ts
+++ src/app/components/shopping-cart/shopping-cart.component.ts
@@ -15,89 +15,84 @@ import { Router } from '@angular/router';
 export class ShoppingCartComponent implements OnInit {
 
     public pruductsInBucket: IProduct[];
-    public isWhatWeDoInTheShadow: IProduct[] = []
+    public isWhatWeDoInTheShadow: IProduct[] =[];
     public th: string[] = ['', 'Product', 'Quantity', 'Price'];
 
-    private uniqueObject = {};
     public totalAmount: number;
     private uid: string = localStorage.getItem('uid');
     private destroyed$: Subject<boolean> = new Subject();
 
-    
 
     constructor(
-        private service: DataService, 
-        private router: Router, 
-        private db: AngularFirestore) { }
+        private service: DataService,
+        private router: Router,
+        private db: AngularFirestore
+    ) { }
 
     public getBucket(): void {
         this.service.getItem('userBucket', this.uid)
             .pipe(takeUntil(this.destroyed$))
             .subscribe((response: any) => {
-                if (response && response.items.length) {
-                    const { items } = response;
-                    this.isWhatWeDoInTheShadow = items;
-                    this.pruductsInBucket = this.removeDuplicates(items);
-                    this.getTotalAmount(this.pruductsInBucket);
-                }
+                const { items } = response;
+                this.isWhatWeDoInTheShadow = items;
+                const data = this.groupBy(this.isWhatWeDoInTheShadow, 'title');
+
+                this.isWhatWeDoInTheShadow.forEach((obj: IProduct) => {
+                    obj.quantity = data[obj.title]?.length;
+                });
+
+                this.pruductsInBucket = this.removeDuplicates(this.isWhatWeDoInTheShadow);
+                this.getTotalAmount(this.pruductsInBucket);
             });
     }
-    
-    private removeDuplicates(items): any[] {
+
+    private groupBy(objectArray, property) {
+        return objectArray.reduce(function (acc, obj) {
+          let key = obj[property]
+          if (!acc[key]) {
+            acc[key] = []
+          }
+          acc[key].push(obj)
+          return acc
+        }, {})
+      }
+
+    private removeDuplicates(items) {
+        const result = [];
         for (let i = 0; i < items.length; i++) {
             // Extract the title 
             const objTitle = items[i].title;
-            // Use the title as the index 
-            this.uniqueObject[objTitle] = items[i];
-        };
-        return this.objectToArray(this.uniqueObject).map((objProperty) => {
-            return objProperty[1];
-        });
-    }
 
-    private objectToArray(obj): any[][] {
-        return Object.keys(obj).map((key) => {
-            return [key, obj[key]];
-        });
+            if (!result.find(item => item.title === objTitle)) {
+                result.push(items[i])
+            }
+        };        
+        return result;
     }
 
     public addItem(currentProduct: IProduct): void {
-        const clone = { ...currentProduct };
-        const index: number = this.pruductsInBucket.indexOf(currentProduct);
-
-        clone.count = currentProduct.count + 1;
-        clone.id = Date.now().toString();
-        this.pruductsInBucket[index] = clone;
-
-        this.isWhatWeDoInTheShadow.push(clone);
+        currentProduct.id = Date.now().toString();
+        this.isWhatWeDoInTheShadow.push(currentProduct);
         this.isWhatWeDoInTheShadow.forEach((obj, i) => obj.seqN = i + 1);
-
         fsBatchedWrites.default.update(this.db, 'userBucket', this.uid, { items: this.isWhatWeDoInTheShadow });
     }
 
     public removeItem(currentProduct: IProduct): void {
-        const index: number = this.pruductsInBucket.indexOf(currentProduct);
-
         if (currentProduct.count >= 1) {
-            currentProduct.count = currentProduct.count - 1;
-            this.pruductsInBucket[index] = currentProduct;
-
             const indexInBucket: number = this.isWhatWeDoInTheShadow.indexOf(currentProduct);
             this.isWhatWeDoInTheShadow.splice(indexInBucket, 1);
-
             fsBatchedWrites.default.update(this.db, 'userBucket', this.uid, { items: this.isWhatWeDoInTheShadow });
         }
     }
 
-    public getTotalAmount(producsInBucket: IProduct[]): void {
-        const total = producsInBucket.map(obj => obj.price * obj.count);
+    public getTotalAmount(data: IProduct[]): void {
+        const total = data.map(obj => obj.price * obj.quantity);
         const reducer = (accumulator, currentValue) => accumulator + currentValue;
         this.totalAmount = total.reduce(reducer);
     }
 
     public emptyBucket(): void {
         fsBatchedWrites.default.update(this.db, 'userBucket', this.uid, { items: [] });
-        this.isWhatWeDoInTheShadow = [];
     }
 
     public handleClick(data: boolean): void {
